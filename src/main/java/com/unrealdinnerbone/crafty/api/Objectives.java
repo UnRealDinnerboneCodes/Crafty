8aa924ddd350135967976d9798f811fabea3be49
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-12-27 11:25:41 -0600

    Test Again

--- src/main/java/com/unrealdinnerbone/crafty/api/Objectives.java
+++ src/main/java/com/unrealdinnerbone/crafty/api/Objectives.java
@@ -3,7 +3,7 @@ package com.unrealdinnerbone.crafty.api;
 import com.unrealdinnerbone.crafty.score.CraftyObjective;
 import org.bukkit.scoreboard.Objective;
 
-public class Scores {
+public class Objectives {
     public static UpdatedObjective getObjective(Objective score) {
         return new CraftyObjective(score);
     }
