3be17c66265cd8b84e8244760c2ff4f53d85ad6c
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-01-13 20:08:23 -0600

    Cleanup Code

--- src/main/java/com/unrealdinnerbone/crafty/packet/PacketHandler.java
+++ src/main/java/com/unrealdinnerbone/crafty/packet/PacketHandler.java
@@ -3,25 +3,23 @@ package com.unrealdinnerbone.crafty.packet;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
 import com.github.retrooper.packetevents.event.PacketListener;
-import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.chat.RemoteChatSession;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.PublicProfileKey;
 import com.github.retrooper.packetevents.protocol.player.TextureProperty;
 import com.github.retrooper.packetevents.protocol.player.UserProfile;
-import com.github.retrooper.packetevents.util.Vector3d;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientVehicleMove;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerInfoUpdate;
 import com.unrealdinnerbone.crafty.api.events.PlayerInfoEvent;
-import com.unrealdinnerbone.crafty.api.events.PlayerVehicleEvent;
-import com.unrealdinnerbone.crafty.api.player.*;
+import com.unrealdinnerbone.crafty.api.player.Action;
+import com.unrealdinnerbone.crafty.api.player.ChatSession;
+import com.unrealdinnerbone.crafty.api.player.PlayerInfo;
+import com.unrealdinnerbone.crafty.api.player.ProfileKey;
 import io.github.retrooper.packetevents.util.SpigotConversionUtil;
-import net.minecraft.world.phys.Vec3;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.joml.Vector3f;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.UUID;
@@ -33,53 +31,28 @@ public class PacketHandler implements PacketListener {
     public void onPacketSend(PacketSendEvent event) {
         if(event.getPacketType() == PacketType.Play.Server.PLAYER_INFO_UPDATE) {
             WrapperPlayServerPlayerInfoUpdate wrapperPlayServerPlayerInfoUpdate = new WrapperPlayServerPlayerInfoUpdate(event);
-            List<Action> actions = wrapperPlayServerPlayerInfoUpdate.getActions()
-                    .stream()
-                    .map(Convert::convertAction)
-                    .toList();
+            List<Action> actions = new ArrayList<>(wrapperPlayServerPlayerInfoUpdate.getActions().stream().map(Convert::convertAction).toList());
             List<PlayerInfo> playerInfos = wrapperPlayServerPlayerInfoUpdate.getEntries()
                     .stream()
-                    .map(entry -> {
-                        GameMode gameMode = SpigotConversionUtil.toBukkitGameMode(entry.getGameMode());
-                        PlayerProfile gameProfile = Convert.convertProfile(entry.getGameProfile());
-                        ChatSession chatSession = Convert.convertChatSession(entry.getChatSession());
-                        return new PlayerInfo(entry.getLatency(), entry.isListed(), gameMode, gameProfile, entry.getDisplayName(), chatSession);
-                    }).toList();
+                    .map(Convert::convertPlayerInfo).toList();
             UUID uuid = event.getUser().getUUID();
             PlayerInfoEvent playerInfoUpdateEvent = new PlayerInfoEvent(uuid, actions, playerInfos);
             if(playerInfoUpdateEvent.callEvent()) {
-                EnumSet<WrapperPlayServerPlayerInfoUpdate.Action> actions1 = EnumSet.copyOf(playerInfoUpdateEvent.getActions().stream().map(DeConvert::convertAction).toList());
-                wrapperPlayServerPlayerInfoUpdate.setActions(actions1);
-                wrapperPlayServerPlayerInfoUpdate.setEntries(playerInfoUpdateEvent.getPlayerInfos().stream().map(playerInfo -> {
-                    com.github.retrooper.packetevents.protocol.player.GameMode gameMode = SpigotConversionUtil.fromBukkitGameMode(playerInfo.gameMode());
-                    UserProfile gameProfile = DeConvert.convertProfile(playerInfo.playerProfile());
-                    return new WrapperPlayServerPlayerInfoUpdate.PlayerInfo(gameProfile, playerInfo.listed(), playerInfo.latency(), gameMode, playerInfo.displayName(), DeConvert.convertChatSession(playerInfo.chatSession()));
-
-                }).toList());
+                wrapperPlayServerPlayerInfoUpdate.setActions(EnumSet.copyOf(playerInfoUpdateEvent.getActions().stream().map(DeConvert::convertAction).toList()));
+                wrapperPlayServerPlayerInfoUpdate.setEntries(playerInfoUpdateEvent.getPlayerInfos().stream()
+                        .map(DeConvert::convertPlayerInfo).toList());
             }
-        }
-    }
-
-    @Override
-    public void onPacketReceive(PacketReceiveEvent event) {
-        if(event.getPacketType() == PacketType.Play.Client.VEHICLE_MOVE) {
-            WrapperPlayClientVehicleMove wrapperPlayClientVehicleMove = new WrapperPlayClientVehicleMove(event);
-            Vector3d position = wrapperPlayClientVehicleMove.getPosition();
-            float yaw = wrapperPlayClientVehicleMove.getYaw();
-            float pitch = wrapperPlayClientVehicleMove.getPitch();
-            UUID uuid = event.getUser().getUUID();
-            Vector3f vec3 = new Vector3f((float) position.getX(), (float) position.getY(), (float) position.getZ());
-            PlayerVehicleEvent playerVehicleEvent = new PlayerVehicleEvent(uuid, vec3, yaw, pitch);
-            if(playerVehicleEvent.callEvent()) {
-                wrapperPlayClientVehicleMove.setPosition(new Vector3d(playerVehicleEvent.getVec3().x, playerVehicleEvent.getVec3().y, playerVehicleEvent.getVec3().z));
-                wrapperPlayClientVehicleMove.setYaw(playerVehicleEvent.getYaw());
-                wrapperPlayClientVehicleMove.setPitch(playerVehicleEvent.getPitch());
-            }
-
         }
     }
 
     public static class Convert {
+
+        public static PlayerInfo convertPlayerInfo(WrapperPlayServerPlayerInfoUpdate.PlayerInfo playerInfo) {
+            GameMode gameMode = SpigotConversionUtil.toBukkitGameMode(playerInfo.getGameMode());
+            PlayerProfile gameProfile = Convert.convertProfile(playerInfo.getGameProfile());
+            ChatSession chatSession = Convert.convertChatSession(playerInfo.getChatSession());
+            return new PlayerInfo(playerInfo.getLatency(), playerInfo.isListed(), gameMode, gameProfile, playerInfo.getDisplayName(), chatSession);
+        }
         public static Action convertAction(WrapperPlayServerPlayerInfoUpdate.Action action) {
             return Action.valueOf(action.name());
         }
@@ -102,6 +75,12 @@ public class PacketHandler implements PacketListener {
     }
 
     public static class DeConvert {
+
+        public static WrapperPlayServerPlayerInfoUpdate.PlayerInfo convertPlayerInfo(PlayerInfo playerInfo) {
+            com.github.retrooper.packetevents.protocol.player.GameMode gameMode = SpigotConversionUtil.fromBukkitGameMode(playerInfo.gameMode());
+            UserProfile gameProfile = DeConvert.convertProfile(playerInfo.playerProfile());
+            return new WrapperPlayServerPlayerInfoUpdate.PlayerInfo(gameProfile, playerInfo.listed(), playerInfo.latency(), gameMode, playerInfo.displayName(), DeConvert.convertChatSession(playerInfo.chatSession()));
+        }
 
         public static WrapperPlayServerPlayerInfoUpdate.Action convertAction(Action action) {
             return WrapperPlayServerPlayerInfoUpdate.Action.valueOf(action.name());
